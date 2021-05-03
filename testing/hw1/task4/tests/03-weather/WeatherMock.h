409c85bd3edb4894b275c75855f461936d06e80f
2toThe10th <grib.a@phystech.edu>
2021-05-04 00:56:01 +0300

    finished

--- testing/hw1/task4/tests/03-weather/WeatherMock.h
+++ testing/hw1/task4/tests/03-weather/WeatherMock.h
@@ -8,6 +8,15 @@
 #include <Weather.h>
 
 class WeatherMock : public Weather {
+ public:
+  MOCK_METHOD(cpr::Response, Get, (const std::string&, const cpr::Url&), (override));
+  virtual cpr::Response JustGet(const std::string& city, const cpr::Url& url);
+};
+
+class WeatherMockWithTemperature : public Weather {
+ public:
+  MOCK_METHOD(float, GetTemperature, (const std::string&), (override));
+  MOCK_METHOD(float, GetTomorrowTemperature, (const std::string&), (override));
 };
 
 
