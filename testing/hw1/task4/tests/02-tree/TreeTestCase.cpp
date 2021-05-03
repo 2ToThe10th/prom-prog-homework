6d5756442d8e4da134bdc0f83287814aa18d6991
2toThe10th <grib.a@phystech.edu>
2021-05-04 01:17:55 +0300

    fixes

--- testing/hw1/task4/tests/02-tree/TreeTestCase.cpp
+++ testing/hw1/task4/tests/02-tree/TreeTestCase.cpp
@@ -70,7 +70,7 @@ void SortNodeByName(FileNode &node) {
   std::sort(node.children.begin(), node.children.end(), [](FileNode &a, FileNode &b) {
     return a.name < b.name;
   });
-  for (auto& children: node.children) {
+  for (auto &children: node.children) {
     SortNodeByName(children);
   }
 }
@@ -82,22 +82,112 @@ TEST_F(TreeTestCase, BigDir) {
   boost::filesystem::create_directories(dir.string());
   auto file = root_dir / boost::filesystem::unique_path();
   boost::filesystem::ofstream(file.string());
-  auto dir_dir1 = dir / boost::filesystem::unique_path();
-  boost::filesystem::create_directories(dir_dir1.string());
-  auto dir_file1 = dir / boost::filesystem::unique_path();
-  boost::filesystem::ofstream(dir_file1.string());
   auto dir_file2 = dir / boost::filesystem::unique_path();
   boost::filesystem::ofstream(dir_file2.string());
+  auto dir_file3 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file3.string());
+  auto dir_dir1 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir1.string());
+  auto dir_dir2 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir2.string());
+  auto dir_file1 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file1.string());
+  auto dir_file4 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file4.string());
+  auto dir_dir3 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir3.string());
+  auto dir_dir4 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir4.string());
+  auto dir_file7 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file7.string());
+  auto dir_file8 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file8.string());
+  auto dir_dir5 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir5.string());
+  auto dir_dir6 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir6.string());
+  auto dir_file5 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file5.string());
+  auto dir_file6 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file6.string());
   auto node = GetTree(root_dir.string(), false);
   SortNodeByName(node);
   FileNode expected{root_dir.filename().string(), true,
                     {{dir.filename().string(), true,
-                      {{dir_dir1.filename().string(), true, {}},
-                       {dir_file1.filename().string(), false, {}},
-                       {dir_file2.filename().string(), false, {}},
+                      {
+                          {dir_dir1.filename().string(), true, {}},
+                          {dir_dir2.filename().string(), true, {}},
+                          {dir_dir3.filename().string(), true, {}},
+                          {dir_dir4.filename().string(), true, {}},
+                          {dir_dir5.filename().string(), true, {}},
+                          {dir_dir6.filename().string(), true, {}},
+                          {dir_file1.filename().string(), false, {}},
+                          {dir_file2.filename().string(), false, {}},
+                          {dir_file3.filename().string(), false, {}},
+                          {dir_file4.filename().string(), false, {}},
+                          {dir_file5.filename().string(), false, {}},
+                          {dir_file6.filename().string(), false, {}},
+                          {dir_file7.filename().string(), false, {}},
+                          {dir_file8.filename().string(), false, {}},
                       }
                      },
                      {file.filename().string(), false, {}}}};
+  SortNodeByName(expected);
+  EXPECT_EQ(node, expected);
+  FilterEmptyNodes(node, root_dir.string());
+  EXPECT_TRUE(boost::filesystem::exists(root_dir.string()));
+  EXPECT_TRUE(boost::filesystem::exists(dir.string()));
+  EXPECT_FALSE(boost::filesystem::exists(dir_dir1.string()));
+}
+
+TEST_F(TreeTestCase, BigDirDirsOnly) {
+  auto root_dir = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(root_dir.string());
+  auto dir = root_dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir.string());
+  auto file = root_dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(file.string());
+  auto dir_file2 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file2.string());
+  auto dir_file3 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file3.string());
+  auto dir_dir1 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir1.string());
+  auto dir_dir2 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir2.string());
+  auto dir_file1 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file1.string());
+  auto dir_file4 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file4.string());
+  auto dir_dir3 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir3.string());
+  auto dir_dir4 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir4.string());
+  auto dir_file7 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file7.string());
+  auto dir_file8 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file8.string());
+  auto dir_dir5 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir5.string());
+  auto dir_dir6 = dir / boost::filesystem::unique_path();
+  boost::filesystem::create_directories(dir_dir6.string());
+  auto dir_file5 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file5.string());
+  auto dir_file6 = dir / boost::filesystem::unique_path();
+  boost::filesystem::ofstream(dir_file6.string());
+  auto node = GetTree(root_dir.string(), true);
+  SortNodeByName(node);
+  FileNode expected{root_dir.filename().string(), true,
+                    {{dir.filename().string(), true,
+                      {
+                          {dir_dir1.filename().string(), true, {}},
+                          {dir_dir2.filename().string(), true, {}},
+                          {dir_dir3.filename().string(), true, {}},
+                          {dir_dir4.filename().string(), true, {}},
+                          {dir_dir5.filename().string(), true, {}},
+                          {dir_dir6.filename().string(), true, {}},
+                      }
+                     }}};
   SortNodeByName(expected);
   EXPECT_EQ(node, expected);
   FilterEmptyNodes(node, root_dir.string());
